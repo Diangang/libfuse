6243ba1e534d9dea2c4c0881fb6f6b28f3ec910b
Bernd Schubert <bschubert@ddn.com>
2025-08-31 21:56:34 +0200

    fuse_loop_mt: Don't create new workers before init is received
    This is to prevent startup races. For example it is
    hard for threads to know the buffer size the kernel usage.
    In the past kernel buffer size always was max 1MB, but in
    the mean time this might be more, depending on system tunings.
    I thought I had fixed all races, but the report in issue #1296
    proves otherwise.
    
    Closes: https://github.com/libfuse/libfuse/issues/1296
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>

--- lib/fuse_loop_mt.c
+++ lib/fuse_loop_mt.c
@@ -173,7 +173,8 @@ static void *fuse_do_work(void *data)
 
 		if (!isforget)
 			mt->numavail--;
-		if (mt->numavail == 0 && mt->numworker < mt->max_threads)
+		if (mt->numavail == 0 && mt->numworker < mt->max_threads &&
+		    likely(se->got_init))
 			fuse_loop_start_thread(mt);
 		pthread_mutex_unlock(&mt->lock);
 
